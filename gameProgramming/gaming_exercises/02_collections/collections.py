7a4edf2d766956542c1635596dd6af1678350ee6
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-10-17 14:16:52 -0400

    v0.100

--- gameProgramming/gaming_exercises/02_collections/collections.py
+++ gameProgramming/gaming_exercises/02_collections/collections.py
@@ -1,13 +1,15 @@
 playerInventory = []
 
 while len(playerInventory) < 10:
-    item = input("What item do you want to ad to the imventory?\n")
+    item = input("What item do you want to add to the imventory?\n")
     playerInventory.append(item)
 
 playerInventory.sort()
 print(playerInventory)
 
-
+when len(playerInventory) > 5:
+    item = input("What item do you want to remove to the imventory?\n")
+    playerInventory.renive(item)
 
 
 
