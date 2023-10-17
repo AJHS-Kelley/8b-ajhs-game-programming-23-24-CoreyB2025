027754be5b972990816c7424ffbe1169a780af1c
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-10-17 14:50:55 -0400

    v0.200

--- gameProgramming/gaming_exercises/02_collections/collections.py
+++ gameProgramming/gaming_exercises/02_collections/collections.py
@@ -1,18 +1,67 @@
-playerInventory = []
+# playerInventory = []
 
-while len(playerInventory) < 10:
-    item = input("What item do you want to add to the imventory?\n")
-    playerInventory.append(item)
+# while len(playerInventory) < 10:
+#     item = input("What item do you want to add to the imventory?\n")
+#     playerInventory.append(item)
 
-playerInventory.sort()
-print(playerInventory)
+# playerInventory.sort()
+# print(playerInventory)
 
-when len(playerInventory) > 5:
-    item = input("What item do you want to remove to the imventory?\n")
-    playerInventory.remove(item)
+# when len(playerInventory) > 5:
+#     item = input("What item do you want to remove to the imventory?\n")
+#     playerInventory.remove(item)
 
-playerInventory.sort()
-print
+# playerInventory.sort()
+# print(playerInventory)
+
+doorKeys = [
+    "red",
+    "green",
+    "yellow",
+    "purple",
+    "brown"
+]
+
+key = input("Which color key do you need to unlock the door?\n")
+
+
+
+
+
+
+
+weaponList[
+False, # shiny sword
+False, # flame thrower
+False, # a gun that shoots more guns
+True, # a grappling hook
+False, # missle launcher
+True, # teleporter beam
+False, # alien blaster
+]
+
+weaponNum = 0
+while weaponNum < len(weaponList):
+    if weaponNum == 0 and weaponList[0] == True:
+        print("You are a wielding a shiny sword.\n")
+    if weaponNum == 1 and weaponList[0] == True:
+        print("You have a toasty flame thrower.\n")
+    if weaponNum == 2 and weaponList[0] == True:
+        print("A gun that shoots more guns in your backpack.\n")
+    if weaponNum == 3 and weaponList[0] == True:
+        print("You can climb walls with this grappling hook.\n")
+    if weaponNum == 4 and weaponList[0] == True:
+        print("A missle launcher is available to use.\n")
+    if weaponNum == 5 and weaponList[0] == True:
+        print("The unique teleporter beam is in your hand.\n")
+    if weaponNum == 6 and weaponList[0] == True:
+        print("You are wielding the alien blaster.\n")
+    weaponNum += 1
+
+
+
+
+
 
 
 
