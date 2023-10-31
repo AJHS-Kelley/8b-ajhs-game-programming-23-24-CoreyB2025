66b71967ba34f092503e1c013ca37629adad3d9f
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-10-31 13:57:54 -0400

    v0.0

---
+++ gameProgramming/gaming_exercises/04b_hangman/hangman.py
@@ -0,0 +1,3 @@
+# Hangman Game by Corey Battle, v0.0
+
+words = 'apple fun run sun bum hum calm pat rice cool potato tomato gringo missle pasta plane insane checker blaster infamous mitochondria phytoplankton piemountaindog epitome coincidental'.split()
