bfeecbc58cddd4dacddf94feba13dddeebdcc69f
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-10-31 14:15:51 -0400

    v0.100

--- gameProgramming/gaming_exercises/04b_hangman/hangman.py
+++ gameProgramming/gaming_exercises/04b_hangman/hangman.py
@@ -1,3 +1,51 @@
 # Hangman Game by Corey Battle, v0.0
 
 words = 'apple fun run sun bum hum calm pat rice cool potato tomato gringo missle pasta plane insane checker blaster infamous mitochondria phytoplankton piemountaindog epitome coincidental'.split()
+
+# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+     +---+
+          |
+          |
+          |
+    ======''', '''
+     +---+
+     O    |
+          |
+        
+    ======''', '''
+     +---+
+     O    |
+     |    |
+          |
+    ======''', '''
+    +---+
+     O    |
+    /|    |
+          |
+    ======''', '''
+    +---+
+     O    |
+    /|\   |
+          |
+    ======''', '''
+     +---+
+     O    |
+    /|\   |
+    /     |
+    ======''', '''
+     +---+
+     O    |
+    /|\   |
+    / \   |
+    ======''']
+                 
+i = 0
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
+
+
+
+
+
