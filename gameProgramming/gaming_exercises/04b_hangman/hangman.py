668d2e4f4caf426cf3838ab68fdeaef339d93914
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-10-31 14:52:01 -0400

    v0.200

--- gameProgramming/gaming_exercises/04b_hangman/hangman.py
+++ gameProgramming/gaming_exercises/04b_hangman/hangman.py
@@ -1,5 +1,5 @@
 # Hangman Game by Corey Battle, v0.0
-
+import random
 words = 'apple fun run sun bum hum calm pat rice cool potato tomato gringo missle pasta plane insane checker blaster infamous mitochondria phytoplankton piemountaindog epitome coincidental'.split()
 
 # VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
@@ -40,10 +40,39 @@ HANGMAN_BOARD = ['''
     / \   |
     ======''']
                  
-i = 0
-while i < len(HANGMAN_BOARD):
-    print(HANGMAN_BOARD[i])
-    i += 1
+# Pick Word from List
+def getRandomWord(wordList): # Return a random word from the list.
+    wordIndex = random.randint(0, len(wordList) -1)
+    # len(listName) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
+    return wordList[wordIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
+    print()
+
+    print('missedLetters:', end = '')
+    for eachLetter in missedLetters:
+        print('eachLetter:', end = '')
+# i = 0
+# while i < 50:
+#    word = getRandomWord(words)
+#    print(word)
+#    i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
