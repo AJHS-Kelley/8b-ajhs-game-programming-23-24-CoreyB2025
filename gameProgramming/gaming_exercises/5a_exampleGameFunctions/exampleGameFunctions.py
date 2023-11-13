73d529b216b611d2829051c108935934558630a0
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-11-13 14:02:19 -0500

    v0.100

--- gameProgramming/gaming_exercises/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/gaming_exercises/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,22 @@
 # Example Game Functions Project, Corey Battle, v0.0
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1): 
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
+
+def shootBall(shotQuality, shotStrength, temperature):
+    if shotQuality > 6.0 and shotStrength >= 76 and temperature >= 60:
+        shotMade = True
+    elif shotQuality > 5.0 and shotStrentgh >= 50 and temperature <= 55:
+        shotMade = False
+
+shootBall(5.6, 72, 68)
