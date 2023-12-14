ddf075f4fdf99f667fe3995166773844e3987c40
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2023-12-14 13:39:25 -0500

    v0.0

---
+++ gameProgramming/gaming_exercises/8B/dice.py
@@ -0,0 +1,20 @@
+# Dice Rolling Module By Corey Battle, v0.0
+import random
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+    return sum
+
+
+
+
+
+
+
