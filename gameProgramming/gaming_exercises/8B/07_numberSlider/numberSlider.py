78f06abee7bc7bd6d182e98f63de3b40d16a8bea
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2024-01-22 13:53:37 -0500

    v0.1

---
+++ gameProgramming/gaming_exercises/8B/07_numberSlider/numberSlider.py
@@ -0,0 +1,8 @@
+# Number Slider, by Corey Battle, based on a project by AI Sweigrt, v0.0
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e. Operating System Commands)
+
+from pygame.locals import *
+# Allows us to call functions from pygame using just the function name instead of module.function()
+# Example: We can use draw() instead of pygame.draw()
