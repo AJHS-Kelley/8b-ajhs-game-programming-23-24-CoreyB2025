9502110635460fbd69136effd7918b75ef9df9f1
CoreyB2025 <111382932+CoreyB2025@users.noreply.github.com>
2024-01-23 03:47:34 -0500

    v0.0

---
+++ gameProgramming/gaming_exercises/8B/cohort_lessons/dnaGame.py
@@ -0,0 +1,14 @@
+dnaGame Corey Battle, v0.0
+
+# Import Entire Modules -- Get the whole tool box.
+import time, datetime 
+
+# Import Specific Methods -- Get the specific tool.
+from random import choice
+
+# Store the DNA Bases
+dnaBases = ["A", "T", "G", "C"]
+
+# GAME FUNCTIONS
+def gameIntro() -> None:
+    pass
